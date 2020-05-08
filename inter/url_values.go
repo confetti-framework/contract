caf11d3a64a96c83719e59d4f735b095e6630109
Reindert Vetter <reindert@myparcel.nl>
2020-05-08 18:27:15 +0200

    Implement adapters to transform requests

--- inter/url_values.go
+++ inter/url_values.go
@@ -4,8 +4,12 @@ import "net/url"
 
 type UrlValues interface {
 	Source() url.Values
-	GetString(key string) string
-	GetStringE(key string) (string, error)
-	GetInt(key string) int
-	GetIntE(key string) (int, error)
+	String(key string) string
+	StringE(key string) (string, error)
+	Strings(key string) []string
+	StringsE(key string) ([]string, error)
+	Number(key string) int
+	NumberE(key string) (int, error)
+	Numbers(key string) []int
+	NumbersE(key string) ([]int, error)
 }
