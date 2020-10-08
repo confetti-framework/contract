56c7e0eac39f1a35dff04ae5ac7a8323dc143f5b
Reindert Vetter <reindert@myparcel.nl>
2020-10-08 22:24:55 +0200

    lanvard/lanvard#68 init stack loggers

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/lanvard/support v0.1.0
+	github.com/lanvard/syslog v0.0.0-20201006215111-98d4d91dbaa8
 )
 
 replace github.com/lanvard/support v0.1.0 => ../support
+replace github.com/lanvard/syslog v0.0.0-20201006215111-98d4d91dbaa8 => ../syslog
--- inter/log_channel.go
+++ inter/log_channel.go
@@ -1,4 +1,29 @@
 package inter
 
-type LogChannel interface {
+import (
+	"github.com/lanvard/syslog"
+)
+
+type Severity = syslog.Priority
+type Facility = syslog.Priority
+
+type Logger interface {
+	Log(severity Severity, message string)
+	LogWith(severity Severity, message string, data interface{})
+	Emergency(message string)
+	EmergencyWith(message string, data interface{})
+	Alert(message string)
+	AlertWith(message string, data interface{})
+	Critical(message string)
+	CriticalWith(message string, data interface{})
+	Error(message string)
+	ErrorWith(message string, data interface{})
+	Warning(message string)
+	WarningWith(message string, data interface{})
+	Notice(message string)
+	NoticeWith(message string, data interface{})
+	Info(message string)
+	InfoWith(message string, data interface{})
+	Debug(message string)
+	DebugWith(message string, data interface{})
 }
