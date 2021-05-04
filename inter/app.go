8b03781743a06bc9e72ee6b2488d566dea933bb7
Reindert Vetter <reindert@myparcel.nl>
2021-05-04 22:52:07 +0200

    #confetti-framework/confetti#131 support db query

--- inter/app.go
+++ inter/app.go
@@ -5,25 +5,30 @@ const AppProvider = "app_provider"
 type App interface {
 	AppReader
 
-	// GetE the service container
+	// Container returns the service container
 	Container() *Container
 
-	// Set the service container
+	// SetContainer set the service container
 	SetContainer(container Container)
 
-	// Register a shared binding in the container.
+	// Singleton registered a shared binding in the container.
 	Singleton(abstract interface{}, concrete interface{})
 
-	// Register an existing instance as shared in the container.
+	// Bind registered an existing instance as shared in the container.
 	Bind(abstract interface{}, concrete interface{})
 
-	// Register an existing instance as shared in the container without an abstract
+	// Instance registered an existing instance as shared in the container without an abstract
 	Instance(concrete interface{}) interface{}
 
 	Environment() (string, error)
 
 	IsEnvironment(environments ...string) bool
 
-	// The Log method gives you an instance of a logger. You can write your log messages to this instance.
+	// The Log method gives you an instance of a logger. You can write your log
+	// messages to this instance.
 	Log(channels ...string) LoggerFacade
+
+	// Db returns the database facade. If no parameters are provided, it will use
+	// the default connection.
+	Db(connection ...string) Database
 }
